433232b53a38de01b8e6e488c7b345e9b61a44c7
MartinRastrilla <rastrillamartin@gmail.com>
2024-05-15 00:55:10 -0300

    Cambios Paciente
    Cambios en la página principal Paciente y páginas de Editar y Crear. Falta terminar método Editar(PUT) e implementar método borrar(DELETE).

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -2,7 +2,7 @@ const Paciente = require('../models/pacienteModel');
 
 exports.createPaciente = async (req, res) => {
     try {
-      const { nombre_paciente, apellido_paciente, documento_paciente, fecha_nac, sexo_paciente, obra_social, plan, activo } = req.body;
+      const { nombre_paciente, apellido_paciente, documento_paciente, fecha_nac, sexo_paciente, obra_social, plan } = req.body;
       const paciente = await Paciente.create({
         nombre_paciente,
         apellido_paciente,
@@ -10,10 +10,9 @@ exports.createPaciente = async (req, res) => {
         fecha_nac,
         sexo_paciente,
         obra_social,
-        plan,
-        activo
+        plan
       });
-      res.status(201).json({ paciente, message: "Paciente creado exitosamente" });
+      res.redirect('/paciente');
     } catch (error) {
       console.error('Error al crear paciente:', error);
       res.status(500).json({ message: "Error al crear paciente" });
@@ -23,7 +22,7 @@ exports.createPaciente = async (req, res) => {
 exports.obtenerPacientes = async (req,res) => {
     try {
         const pacientes = await Paciente.findAll();
-        res.status(200).json(pacientes);
+        res.render('pages/pacienteViews/paciente', {pacientes});
     } catch (error) {
         console.error('Error al buscar pacientes.', error);
         res.status(500).json({message:'Error al buscar pacientes.'});
@@ -36,9 +35,20 @@ exports.buscarPacienteByDNI = async (req,res) => {
         const paciente = await Paciente.findOne({ where: { documento_paciente } });
         if (!paciente) {
             const message = "Paciente no Encontrado."
-            return res.render('pages/paciente', {message});
+            return res.render('pages/pacienteViews/paciente', {message});
         }
-        res.render('pages/paciente', {paciente});
+        res.render('pages/pacienteViews/paciente', {paciente});
+    } catch (error) {
+        console.error('Error al obtener paciente por ID:', error);
+        res.status(500).json({ message: "Error al obtener paciente por ID" });
+    }
+};
+
+exports.mostrarEditarPaciente = async (req,res) => {
+    try {
+        const { id_paciente }= req.params;
+        const paciente = await Paciente.findByPk(id_paciente);
+        res.render('pages/pacienteViews/actualizarPaciente', {paciente,create:false});
     } catch (error) {
         console.error('Error al obtener paciente por ID:', error);
         res.status(500).json({ message: "Error al obtener paciente por ID" });
@@ -47,7 +57,7 @@ exports.buscarPacienteByDNI = async (req,res) => {
 
 exports.actualizarPaciente = async (req, res) => {
     try {
-        const { documento_paciente } = req.query.documento_paciente;
+        const { documento_paciente } = req.params;
         const { nombre_paciente, apellido_paciente, fecha_nac, sexo_paciente, obra_social, plan } = req.body;
         const paciente = await Paciente.findOne({ where: { documento_paciente } });
         if (!paciente) {
--- routes/pacienteRoute.js
+++ routes/pacienteRoute.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const pacienteControl = require('../controllers/pacienteController');
 
 //CRUD PACIENTE
-router.get('/buscar', (req,res) => {
-    res.render('pages/paciente');
+router.get('/crear', async(req,res)=>{
+    res.render('pages/pacienteViews/crearPaciente', {create:true});
 });
+router.post('/crear', pacienteControl.createPaciente);
+
+router.get('/editar/:id_paciente', pacienteControl.mostrarEditarPaciente);
+
 router.get('/', pacienteControl.obtenerPacientes);
-router.get('/buscar-por-documento', pacienteControl.buscarPacienteByDNI);
-router.post('/create', pacienteControl.createPaciente);
-router.put('/actualizar-paciente', pacienteControl.actualizarPaciente);
+router.get('/details', pacienteControl.buscarPacienteByDNI);
+router.put('/details/actualizar/:documento_paciente', pacienteControl.actualizarPaciente);
 
 module.exports = router;
