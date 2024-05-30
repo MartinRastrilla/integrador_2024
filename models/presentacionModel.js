270127d90be6c89c02b7337cf5878b17fba42075
MartinRastrilla <rastrillamartin@gmail.com>
2024-05-30 00:55:32 -0300

    Carga de Medicamentos Completa

--- models/presentacionModel.js
+++ models/presentacionModel.js
@@ -7,65 +7,65 @@ const Categoria = require('../models/categoriaModel');
 const Familia = require('../models/familiaModel');
 
 const Presentacion = sequelize.define('Presentacion', {
-    id_presentacion: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    id_medicamento: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Medicamento,
-        key: 'id_medicamento'
-      }
-    },
-    id_forma: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Forma_farmaceutica,
-        key: 'id_forma'
-      }
-    },
-    concentracion: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-    u_medida: {
-      type: DataTypes.STRING(10),
-      allowNull: false
-    },
-    cantidad_u: {
-      type: DataTypes.INTEGER
-    },
-    id_categoria: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Categoria,
-        key: 'id_categoria'
-      }
-    },
-    id_familia: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Familia,
-        key: 'id_familia'
-      }
-    },
-    activo: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
+  id_presentacion: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  id_medicamento: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Medicamento,
+      key: 'id_medicamento'
     }
-  }, {
-    tableName: 'Presentaciones'
-  });
-  
-  Medicamento.belongsToMany(Categoria, { through: Presentacion, foreignKey: 'id_medicamento' });
-  Categoria.belongsToMany(Medicamento, { through: Presentacion, foreignKey: 'id_categoria' });
-  
-  Medicamento.belongsToMany(Familia, { through: Presentacion, foreignKey: 'id_medicamento' });
-  Familia.belongsToMany(Medicamento, { through: Presentacion, foreignKey: 'id_familia' });
-  
-  Medicamento.belongsToMany(Forma_farmaceutica, { through: Presentacion, foreignKey: 'id_medicamento' });
-  Forma_farmaceutica.belongsToMany(Medicamento, { through: Presentacion, foreignKey: 'id_forma' });
-  
-  module.exports = Presentacion;
+  },
+  id_forma: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Forma_farmaceutica,
+      key: 'id_forma'
+    }
+  },
+  concentracion: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  u_medida: {
+    type: DataTypes.STRING(20),
+    allowNull: false
+  },
+  cantidad_u: {
+    type: DataTypes.INTEGER
+  },
+  id_categoria: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Categoria,
+      key: 'id_categoria'
+    }
+  },
+  id_familia: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Familia,
+      key: 'id_familia'
+    }
+  },
+  activo: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+}, {
+  tableName: 'Presentaciones',
+  indexes: [
+    {
+      unique: true,
+      fields: [
+        'id_medicamento', 'id_forma', 'concentracion', 'u_medida', 'cantidad_u', 'id_categoria', 'id_familia'
+      ],
+      name: 'unique_presentacion'  // Nombre más corto para el índice
+    }
+  ]
+});
+
+module.exports = Presentacion;
